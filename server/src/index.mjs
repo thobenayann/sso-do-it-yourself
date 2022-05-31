5c8eb932dcad678917d0be37a24418a0cdc0bec1
Yann Thobena <thobena.yann@orange.fr>
2022-05-31 10:23:27 +0200

    edit readme and add template for login

--- server/src/index.mjs
+++ server/src/index.mjs
@@ -2,12 +2,27 @@
 // m pour module
 // cela va permettre d'utiliser les imports, ect des modules
 // Et non pas les "requires"
+// A ajouter également "--experimental-modules" dans le script
+import path from 'path';
 import express from 'express';
+
+// mise en place de la variable "__dirname" quand on a un module
+const __dirname = path.resolve();
 
 const app = express();
 
+// Activation du moteur de template PUG
+app.set("view engine", "pug");
+app.set("views", "./src/views");
+
+// CSS
+app.use(
+  '/public/stylesheets',
+  express.static(__dirname + '/public/stylesheets')
+);
+
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.render("login")
 });
 
 app.listen(3000, () => {
